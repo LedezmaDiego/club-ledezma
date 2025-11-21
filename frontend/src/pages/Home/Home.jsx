a3c2a9035f874421c61310f91e91cecc68aba749
Diego Ajata Ledezma <ajataledezmadiego@gmail.com>
2025-11-21 17:29:42 -0300

    feat: se agrego funcionalidad home

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h1>Bienvenido a la página principal</h1>
+      <p>Esta es una página de ejemplo usando React + Vite.</p>
+    </div>
+  );
+}
